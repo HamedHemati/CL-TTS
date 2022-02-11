e6f05e3123f7f770ca81f4ae529a39bc969e0826
hamedhemati <hemati.hmd@gmail.com>
2022-02-11 23:49:29 +0100

    clean `MultiSpeakerDataset`

--- cl_tts/benchmarks/datasets/multispeaker_dataset.py
+++ cl_tts/benchmarks/datasets/multispeaker_dataset.py
@@ -1,21 +1,7 @@
 import os
-import glob
-
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torchaudio
-import numpy as np
-
-
-from avalanche.benchmarks.utils.avalanche_dataset import AvalancheDataset, \
-    AvalancheSubset
-
-from cl_tts.utils.ap import AudioProcessor
-from cl_tts.utils.generic import load_params
-from .dataset_utils.sampler import BinnedLengthSampler
-from .dataset_utils.collator import TTSColator
-from .dataset_utils.text_processors.eng.english_text_proessor import \
-    EnglishTextProcessor
 
 
 class MultiSpeakerDataset(Dataset):
@@ -82,7 +68,7 @@ class MultiSpeakerDataset(Dataset):
         if ext in ["wav"]:
             return self.ap.load_audio(file_path)
         else:
-            NotImplementedError
+            raise NotImplementedError
 
     def get_durations(self, indices):
         durations = []
@@ -113,87 +99,3 @@ class MultiSpeakerDataset(Dataset):
 
     def __len__(self):
         return len(self.data)
-
-
-def get_multispeaker_dataloader():
-    def get_dataloader(text_processor,
-                       eval=False,
-                       **params):
-            # Get dataset
-        dataset = TTSDataset(text_processor, eval=eval, **params)
-
-        # Get transcript lengths for the sampler
-        audio_durations = dataset.get_audio_durations()
-
-        # Define sampler
-        if params["sampler"] == "binned_length_sampler" and not eval:
-            sampler = BinnedLengthSampler(audio_durations, params["batch_size"],
-                                          params["batch_size"])
-        else:
-            sampler = None
-
-        # Create dataloader
-        collator = TTSColator(
-            reduction_factor=params["model"]["n_frames_per_step"],
-            audio_params=params["audio_params"])
-
-        dataloader = DataLoader(dataset,
-                                collate_fn=collator,
-                                batch_size=batch_size,
-                                sampler=sampler,
-                                num_workers=params["num_workers"],
-                                drop_last=False,
-                                pin_memory=True,
-                                shuffle=False)
-
-        return dataloader, dataset.speaker_to_id
-
-
-if __name__ == "__main__":
-    datasets_root = "./data/"
-    dataset_name = "VCTK-mini"
-    speaker_list = ["vctk_speaker335", "vctk_speaker336", "vctk_speaker339"]
-
-    ap_params = load_params("hparams/speaker_incremental/naive.yml")["ap_params"]
-    audio_processor = AudioProcessor(ap_params)
-    english_text_processor = EnglishTextProcessor()
-
-    multispk_dataset = MultiSpeakerDataset(
-        datasets_root,
-        dataset_name,
-        speaker_list,
-        audio_processor,
-        data_folder="audios",
-        metafile_name="metadata.txt",
-        file_format="wav",
-        transcript_processor=english_text_processor
-    )
-
-    ds = AvalancheDataset(multispk_dataset)
-
-    target_speaker = "vctk_speaker335"
-    indices = np.where(np.array(ds.targets) == target_speaker)[0]
-    durations = multispk_dataset.get_durations(indices=indices)
-
-    ds_speaker1 = AvalancheSubset(ds, indices=indices)
-
-    batch_size = 32
-    bin_size = 64
-    sampler = BinnedLengthSampler(durations, batch_size, bin_size)
-    collator = TTSColator(1, audio_processor)
-
-    dataloader = DataLoader(ds_speaker1,
-                            collate_fn=collator,
-                            batch_size=batch_size,
-                            sampler=sampler,
-                            num_workers=0,
-                            drop_last=False,
-                            pin_memory=True,
-                            shuffle=False)
-
-    for i, (batch, speakers) in enumerate(dataloader):
-        print(batch["transcripts"].shape)
-        print(batch["trans_lengths"].shape)
-        print(batch["melspecs"].shape)
-        print(batch["melspec_lengths"].shape)
-        break
