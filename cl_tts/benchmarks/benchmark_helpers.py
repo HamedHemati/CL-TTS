17dd509f38256ac0e985c606a794dc0a7e5edf31
hamedhemati <hemati.hmd@gmail.com>
2022-02-12 01:16:07 +0100

    Add `VCTK-Speaker-Inc` benchmark

--- cl_tts/benchmarks/benchmark_helpers.py
+++ cl_tts/benchmarks/benchmark_helpers.py
@@ -1,11 +1,20 @@
+from .vctk_speaker_inc import get_vctk_speaker_incremental_benchmark
 
 
 def get_benchmark(args, params):
-    # -----> Tacotron2NV
-    if params["benchmark"] == "CSS10":
-        benchmark = None
+    benchmark, benchmark_meta = None, None
+    if params["benchmark"] == "VCTK-Speaker-Inc":
+        benchmark, benchmark_meta = get_vctk_speaker_incremental_benchmark(
+            params["datasets_root"],
+            params["dataset_name"],
+            params["data_folder"],
+            params["metafile_name"],
+            params["file_format"],
+            params["speaker_list"],
+            params["ap_params"]
+        )
 
     else:
         raise NotImplementedError()
 
-    return benchmark
+    return benchmark, benchmark_meta
---
+++ cl_tts/benchmarks/vctk_speaker_inc.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+from avalanche.benchmarks.generators import dataset_benchmark
+from avalanche.benchmarks.utils.avalanche_dataset import AvalancheDataset, \
+    AvalancheSubset
+
+from cl_tts.utils.ap import AudioProcessor
+from cl_tts.benchmarks.datasets.multispeaker_dataset import MultiSpeakerDataset
+from cl_tts.benchmarks.datasets.dataset_utils.text_processors.\
+    eng.english_text_proessor import EnglishTextProcessor
+
+
+def get_vctk_speaker_incremental_benchmark(
+        datasets_root,
+        dataset_name,
+        data_folder,
+        metafile_name,
+        file_format,
+        speaker_list,
+        audio_params,
+):
+    # Audio processor
+    audio_processor = AudioProcessor(audio_params)
+
+    # Text processor
+    english_text_processor = EnglishTextProcessor()
+
+    speaker_datasets = []
+    speaker_durations = []
+    for target_speaker in speaker_list:
+        # Create dataset
+        multispk_dataset = MultiSpeakerDataset(
+            datasets_root,
+            dataset_name,
+            speaker_list,
+            audio_processor,
+            data_folder=data_folder,
+            metafile_name=metafile_name,
+            file_format=file_format,
+            transcript_processor=english_text_processor
+        )
+        ds = AvalancheDataset(multispk_dataset)
+
+        # Select indices for the target speaker
+        indices = np.where(np.array(ds.targets) == target_speaker)[0]
+
+        # Compute durations
+        durations = multispk_dataset.get_durations(indices=indices)
+        speaker_durations.append(durations)
+
+        # Target speaker dataset
+        ds_target_speaker = AvalancheSubset(ds, indices=indices)
+        speaker_datasets.append(ds_target_speaker)
+
+    benchmark = dataset_benchmark(speaker_datasets, speaker_datasets)
+    benchmark_meta = {
+        "speaker_durations": speaker_durations,
+        "n_symbols": len(english_text_processor.symbols),
+        "n_speakers": len(speaker_list)
+    }
+    return benchmark, benchmark_meta
