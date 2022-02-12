7bd2b75fe8a5ac79823065ffe1a93056930354f0
hamedhemati <hemati.hmd@gmail.com>
2022-02-12 01:17:40 +0100

    Initialize `spekaer_inc_trainer`

---
+++ cl_tts/trainers/speaker_inc_trainer.py
@@ -0,0 +1,11 @@
+from .base_trainer import BaseTrainer
+
+
+class Trainer(BaseTrainer):
+    def __init__(self, args, params, experiment_name):
+        super().__init__(args, params, experiment_name)
+
+    def run(self):
+        for exp in self.benchmark.train_stream:
+            # self.strategy.train(exp)
+            print(set(exp.dataset.targets))
